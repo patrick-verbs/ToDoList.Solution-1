5728015f6053366a540f359e05d3710988d3ccdf
yesthecarlos <carlosmendez86@gmail.com>
2021-05-05 14:50:56 -0700

    Add Program class for UI
    Co-authored-by: Cassandra Copp <cass16180@gmail.com>
    Co-authored-by: Seth Medeiros <Medeirosseth@gmail.com>

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,7 +1,26 @@
+using System.Collections.Generic;
+
 namespace ToDoList.Models
+
 {
   public class Item
   {
-    //properties, methods, etc. will go here.
+    public string Description { get; set; }
+    private static List<Item> _instances = new List<Item> {};
+
+    public Item(string description)
+    {
+      Description = description;
+      _instances.Add(this);
+    }
+    public static List<Item> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
   }
 }
---
+++ ToDoList/Program.cs
@@ -0,0 +1,54 @@
+using System;
+using ToDoList.Models;
+using System.Collections.Generic;
+
+
+namespace ToDoList.Models
+{
+  public class Program
+  {
+
+    public static void Main()
+    {
+      List<Item> items = Item.GetAll();
+      Console.WriteLine("Welcome to the To-Do list press 'Y' to proceed");
+      string userInput = Console.ReadLine();
+      string lowerCase = userInput.ToLower();
+      if(lowerCase == "y")
+      {
+        addViewLoop();
+      }
+      else
+      {
+        Main();
+      }    
+    }
+      static void addViewLoop() 
+      {
+        List<Item> items = Item.GetAll();
+      Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View");
+      string userAnswer = Console.ReadLine();
+
+      if(userAnswer == "Add" || userAnswer == "add")
+      {
+        Console.WriteLine("Enter item you would like add: ");
+        string newUserItem = Console.ReadLine();
+        Item newItem = new Item(newUserItem);
+        Console.WriteLine(newItem.Description + " has been added to your list.");
+        addViewLoop();
+      }
+      else if(userAnswer == "View" || userAnswer == "view")
+      {
+        foreach (Item thisItem in items)
+        {
+          Console.WriteLine(thisItem.Description);
+        }
+        addViewLoop();
+      }
+      else
+      {
+        addViewLoop();
+      }
+    }   
+  }
+}
