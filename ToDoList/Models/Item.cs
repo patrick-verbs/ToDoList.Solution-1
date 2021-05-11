a34f5f7c671bbdb4c2804d23f93ca5a04a08f03e
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-05-10 20:31:36 -0700

    add MVC boiler plate
    Co-authored-by: Tiffany Greathead <tiffanygreathead@gmail.com>
    Co-Authored-By: Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,18 +1,18 @@
 using System.Collections.Generic;
 
 namespace ToDoList.Models
-
 {
   public class Item
   {
     public string Description { get; set; }
     private static List<Item> _instances = new List<Item> {};
 
-    public Item(string description)
+    public Item (string description)
     {
       Description = description;
       _instances.Add(this);
     }
+
     public static List<Item> GetAll()
     {
       return _instances;
@@ -22,5 +22,36 @@ namespace ToDoList.Models
     {
       _instances.Clear();
     }
+
   }
 }
+
+
+
+
+// using System.Collections.Generic;
+
+// namespace ToDoList.Models
+
+// {
+//   public class Item
+//   {
+//     public string Description { get; set; }
+//     private static List<Item> _instances = new List<Item> {};
+
+//     public Item(string description)
+//     {
+//       Description = description;
+//       _instances.Add(this);
+//     }
+//     public static List<Item> GetAll()
+//     {
+//       return _instances;
+//     }
+
+//     public static void ClearAll()
+//     {
+//       _instances.Clear();
+//     }
+//   }
+// }
--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,54 +1,76 @@
-using System;
-using ToDoList.Models;
-using System.Collections.Generic;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
-
-namespace ToDoList.Models
+namespace ToDoList
 {
   public class Program
   {
-
-    public static void Main()
+    public static void Main(string[] args)
     {
-      List<Item> items = Item.GetAll();
-      Console.WriteLine("Welcome to the To-Do list press 'Y' to proceed");
-      string userInput = Console.ReadLine();
-      string lowerCase = userInput.ToLower();
-      if(lowerCase == "y")
-      {
-        addViewLoop();
-      }
-      else
-      {
-        Main();
-      }    
-    }
-      static void addViewLoop() 
-      {
-        List<Item> items = Item.GetAll();
-      Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View");
-      string userAnswer = Console.ReadLine();
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
 
-      if(userAnswer == "Add" || userAnswer == "add")
-      {
-        Console.WriteLine("Enter item you would like add: ");
-        string newUserItem = Console.ReadLine();
-        Item newItem = new Item(newUserItem);
-        Console.WriteLine(newItem.Description + " has been added to your list.");
-        addViewLoop();
-      }
-      else if(userAnswer == "View" || userAnswer == "view")
-      {
-        foreach (Item thisItem in items)
-        {
-          Console.WriteLine(thisItem.Description);
-        }
-        addViewLoop();
-      }
-      else
-      {
-        addViewLoop();
-      }
-    }   
+      host.Run();
+    }
   }
 }
+
+
+
+
+// using System;
+// using ToDoList.Models;
+// using System.Collections.Generic;
+// namespace ToDoList.Models
+// {
+//   public class Program
+//   {
+
+//     public static void Main()
+//     {
+//       List<Item> items = Item.GetAll();
+//       Console.WriteLine("Welcome to the To-Do list press 'Y' to proceed");
+//       string userInput = Console.ReadLine();
+//       string lowerCase = userInput.ToLower();
+//       if(lowerCase == "y")
+//       {
+//         addViewLoop();
+//       }
+//       else
+//       {
+//         Main();
+//       }    
+//     }
+//       static void addViewLoop() 
+//       {
+//         List<Item> items = Item.GetAll();
+//       Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View");
+//       string userAnswer = Console.ReadLine();
+
+//       if(userAnswer == "Add" || userAnswer == "add")
+//       {
+//         Console.WriteLine("Enter item you would like add: ");
+//         string newUserItem = Console.ReadLine();
+//         Item newItem = new Item(newUserItem);
+//         Console.WriteLine(newItem.Description + " has been added to your list.");
+//         addViewLoop();
+//       }
+//       else if(userAnswer == "View" || userAnswer == "view")
+//       {
+//         foreach (Item thisItem in items)
+//         {
+//           Console.WriteLine(thisItem.Description);
+//         }
+//         addViewLoop();
+//       }
+//       else
+//       {
+//         addViewLoop();
+//       }
+//     }   
+//   }
+// }
